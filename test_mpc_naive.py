7298d7ddf8c42cdcf870c9048aa1f74df0544719
Kinan Dak Albab <kinan.bab.14@gmail.com>
2017-02-25 18:25:20 -0500

    bench2

--- test_mpc_naive.py
+++ test_mpc_naive.py
@@ -46,7 +46,7 @@ INITIALIZATION
 """
 time_ = time()
 
-directory = "graphs/AS_OREGON/bench1"
+directory = "graphs/AS_OREGON/bench2"
 ID = sys.argv[2]
 ID = int(ID[ID.index("-")+1:ID.index(".")])
 ID = str(ID)
